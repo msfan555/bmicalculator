bd3a209de4b427c68da61c955cc85ac1bc1ab280
Fan <yfctw555@gmail.com>
2022-06-19 18:29:45 +0800

    使用get傳值的bmi計算機

---
+++ calculating.php
@@ -0,0 +1,26 @@
+<?php
+
+$height=$_GET['height'];
+$weight=$_GET['weight'];
+
+$bmi=round($weight/(($height/100)*($height/100)),1);
+
+$result="";
+
+if ($bmi < 18.5) {
+    $result="體重過輕，要多攝取一些營養的食物喔！";
+} else if ($bmi >= 18.5 && $bmi < 24) {
+    $result="標準體重，請繼續保持健康喔！";
+} else if ($bmi >= 24 && $bmi < 27) {
+    $result="體重過重，要健康飲食和運動喔！";
+} else if ($bmi >= 27 && $bmi < 30){
+    $result="輕度肥胖，肥胖是很多疾病的根源，要健康飲食和運動喔！";
+}else if($bmi >= 30 && $bmi < 35){
+    $result="中度肥胖，肥胖是很多疾病的根源，要健康飲食和運動喔！";
+}else{
+    $result="重度肥胖，肥胖是很多疾病的根源，要健康飲食和運動喔！";
+}
+
+header("location:result.php?bmi=$bmi&result=$result");
+
+?>
---
+++ result.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMI計算結果</title>
+</head>
+
+<body>
+
+    <h1 style="font-size:3rem;text-align:center">
+    BMI為 <?=$_GET['bmi'];?>
+    </h1>
+    <h2 style="font-size:3rem;text-align:center">
+    <?=$_GET['result'];?>
+    </h2>
+    <div style="font-size:3rem;text-align:center">
+        <a href="bmi.html"><button>回到BMI計算機</button></a>
+    </div>
+
+</body>
+
+</html>
