c3ad2bf052b70713acbf2600c3aab9398c777a11
Fan <yfctw555@gmail.com>
2022-06-21 19:23:13 +0800

    session模擬購物車

--- cart.php
+++ cart.php
@@ -1,12 +1,24 @@
 <h1>cart</h1>
 <?php
-setcookie('產品',serialize(["貓跳台"=>1,"貓碗"=>2,"牛奶"=>3,"逗貓棒"=>4]),time()+3600);
+// cookie
+// setcookie('產品',serialize(["貓跳台"=>1,"貓碗"=>2,"牛奶"=>3,"逗貓棒"=>4]),time()+3600);
 
-$cart=unserialize($_COOKIE['產品']);
-foreach($cart as $product => $quantity){
+// $cart=unserialize($_COOKIE['產品']);
+// foreach($cart as $product => $quantity){
+//     echo $product."->".$quantity."<br>";
+// }
+
+
+// session
+session_start();
+$_SESSION['cart']["貓跳台"]=1;
+$_SESSION['cart']["貓碗"]=2;
+$_SESSION['cart']["牛奶"]=3;
+$_SESSION['cart']["逗貓棒"]=4;
+$_SESSION['cart']["小魚乾"]=5;
+
+foreach($_SESSION['cart'] as $product => $quantity){
     echo $product."->".$quantity."<br>";
 }
-
-
 
 ?>
