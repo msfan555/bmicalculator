fe3ea9ba1ac3ba1d601bcd4befb909af42dd7e50
Fan <yfctw555@gmail.com>
2022-06-21 13:46:50 +0800

    cookie做登入頁面

---
+++ session/chklogin.php
@@ -0,0 +1,20 @@
+<?php
+
+$default_user='fan';
+$default_pw="1234";
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$error='';
+
+if($acc!=$default_user || $pw!=$default_pw){
+    $error="帳號密碼錯誤，請回登入頁面重新輸入";
+    header("location:login.php?error=$error");
+}else{
+    setcookie('user',$acc,time()+360);
+    header("location:membercenter.php?user=$acc");
+}
+
+
+?>
